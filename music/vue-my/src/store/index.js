d19d8e3196b75c527583209355f82139924a7adb
lijianjie123 <505604254@qq.com>
2019-06-18 17:05:12 +0800

    修复下一曲功能中hash报错的问题

--- music/vue-my/src/store/index.js
+++ music/vue-my/src/store/index.js
@@ -22,7 +22,7 @@ const store = new Vuex.Store({
 			style: {'background': 'rgba(43,162,251,0)'}
     },
     
-    signIndex:0,
+    //signIndex:0,
     toggleHideV:true,  //true为隐藏
 
     listenCount:0,  // 记录播放过几首歌曲  默认0开始
@@ -33,7 +33,7 @@ const store = new Vuex.Store({
     //所有歌曲信息
     listInfo:{
       songs:[], //歌曲xinxi
-      songIndex:'0' , // 默认第一首歌曲索引
+      songIndex: ' ' , // 默认第一首歌曲索引
      // firstSongInfo:[],
     },
     isPlay: false,
@@ -116,7 +116,7 @@ const store = new Vuex.Store({
      
       // axios.get(`/proxy/app/i/krc.php?cmd=100&keyword=${info.songname}&${info.hash}`).then(({data})=>{
         axios.get(`/proxy/api/v1/song/get_song_info?cmd=playInfo&hash=${info.hash}`).then(({data})=>{
-          console.log('store  getsongd()方法 ---请求成功')
+          console.log('store  getsong ()方法 ---请求成功')
           console.log(data)
         //  data中 找到播放歌曲信息  需要用到的数据（默认状态 在state 的audio中定义了）
         const songUrl = data.url
@@ -129,25 +129,7 @@ const store = new Vuex.Store({
         commit('setAudio', {audio})
       })
     },
-    next({dispatch, state}, bool){
-        let list = state.listInfo.songs
-        let	index = ++state.listInfo.songIndex 
-        console.log('next index初始赋值---' ,index)
-        if(index == list.length ){
-            index = 0 
-            state.listInfo.songIndex = 0
-        } 
-        let hash = list.filter((item,key,self)=>{
-          console.log('next---' ,key)
-          return key == index
-        })[0].hash
-        console.log(hash)
-        let info = {index, hash}
-      dispatch('getSong', info)
-     
-    },
-
-
+    
     prev({dispatch, state}){
       let list = state.listInfo.songs
       let index = --state.listInfo.songIndex
@@ -166,7 +148,29 @@ const store = new Vuex.Store({
       
       let info = {index, hash}
       dispatch('getSong', info)
+    },
+    next({dispatch, state}, ){
+      let list = state.listInfo.songs
+      //这里有一个hash 报错  然后添加了state.listInfo.songIndex == ' ' 这个判断，
+      // 因为初始状态的时候   没有歌曲数据 获取不到对应的hash值 
+      if(state.listInfo.songIndex == ' '){
+          return false
+      }else{
+        let	index = ++state.listInfo.songIndex 
+        console.log('next index初始赋值---' ,index)
+        if(index == list.length ){
+            index = 0 
+            state.listInfo.songIndex = 0
+        } 
+        let hash = list.filter((item,key,self)=>{
+          console.log('next---' ,key)
+          return key == index
+        })[0].hash
+        console.log(hash)
+        let info = {index, hash}
+        dispatch('getSong', info)
     }
+  },
 
   }
 
